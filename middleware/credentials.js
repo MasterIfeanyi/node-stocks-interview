349318b4d6dcec67a19876387f72f5c1f7fc05df
Ifeanyi <chimaifeanyi29@gmail.com>
2022-07-29 00:12:02 +0100

    add cors headers

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,7 +3,10 @@ const allowedOrigins = require("../config/allowedOrigins");
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Credentials", true)
+        res.set({
+            "Access-Control-Allow-Origin": origin,
+            "Access-Control-Allow-Credentials": true
+        })
     }
     next();
 }
